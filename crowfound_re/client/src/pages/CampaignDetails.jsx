daedf9e2ddfd21777401831c1b53bdecb2189ad3
ttu777 <138754539+ttu114514@users.noreply.github.com>
2023-08-14 10:28:52 +0800

    Update CampaignDetails.jsx

--- crowfound_re/client/src/pages/CampaignDetails.jsx
+++ crowfound_re/client/src/pages/CampaignDetails.jsx
@@ -39,7 +39,7 @@ const CampaignDetails = () => {
 
       <div className='w-full flex md:flex-row flex-col mt-10 gap-[30px]'>
         <div className='flex-1 flex-col'>
-          <img src={state.image} alt='campaign' className='w-full h-[410px] object-cover rounded-[10px]' />
+          <img src={state.image} alt='campaign' className='w-full h-[410px] object-contain rounded-[10px]' />
           <div className='relative w-full h-[5px] bg-[#3a3a43] mt-2'>
             <div className='absolute h-full bg-[#4acd8d]' style={{ width: `${calculateBarPercentage(state.target, state.amountCollected)}%`, maxWidth: '100%' }}>
 
