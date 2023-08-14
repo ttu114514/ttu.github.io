ae467a51447c23c5579d7405ab162c95f3b9b195
ttu777 <138754539+ttu114514@users.noreply.github.com>
2023-08-14 10:20:48 +0800

    Update FundCard.jsx

--- crowfound_re/client/src/components/FundCard.jsx
+++ crowfound_re/client/src/components/FundCard.jsx
@@ -12,7 +12,7 @@ const FundCard = ({owner, title, description, target, deadline, amountCollected,
   const truncatedAddress = truncateAddress(owner);
   return (
     <div className='sm:w-[280px] w-full rounded-[15px] bg-[#1c1c24] cursor-pointer' onClick={handleClick}>
-        <img src={image} alt='fund' className='w-full h-[158px] object-cover rounded-[15px]'/>
+    <img src={image} alt='fund' className='w-full h-[158px] object-contain rounded-[15px]'/>
 
         <div className='flex flex-col p-4'>
             <div className='flex flex-row items-center mb-[18px]'>
